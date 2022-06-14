1cd036fb30c6a8931008cae61328df462bc3f264
Jeff <salisbury.jeffery@gmail.com>
2022-06-13 23:50:29 -0400

    Checking to see if netlify function is being used

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -2,7 +2,7 @@ exports.handler = async function (event, context, callback) {
   // Pulling out the payload from the body
   const { payload } = JSON.parse(event.body);
 
-  console.log(payload);
+  console.log("PayLoad from Function", payload);
 
   callback(null, {
     statusCode: 200,
